d0413e0f725d8bdef484eb7252ccee91239ce546
nut77 <sunnynut77@gmail.com>
2021-06-23 18:04:06 +0800

    chore-修改vite配置

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus';
+import '@a/styles/index.less';
+import App from './App.vue';
 
+const app = createApp(App);
+app.use(ElementPlus, { size: 'medium' });
 console.log(import.meta.env.MODE);
 console.log(import.meta.env.VITE_AXIOS_BASE_URL);
-createApp(App).mount("#app");
+app.mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+const path = require('path');
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    extensions: ['.js', '.json', '.vue'],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@a': path.resolve(__dirname, './src/assets'),
+      '@p': path.resolve(__dirname, './src/pages'),
+      '@c': path.resolve(__dirname, './src/components'),
+      '@l': path.resolve(__dirname, './src/layout')
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: '8000',
@@ -13,6 +23,18 @@ export default defineConfig({
         target: 'http://api.ifbes.com/mock/20/api',
         changeOrigin: true,
         rewrite: (path) => path.replace('^/api', '')
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          hack: `true; @import (reference) "${resolve(
+            'src/styles/variable.less'
+          )}";`
+        },
+        javascriptEnabled: true
       }
     }
   }
